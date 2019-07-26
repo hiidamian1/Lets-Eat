cd136dc7821cb3a9e1ae2b10b1caa728a718489b
Michael Vo <51869348+vomichaelminh@users.noreply.github.com>
2019-07-26 00:13:44 -0700

    Add files via upload
    updated

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import requests
 import random
 
 app = Flask(__name__)
 
 
-@app.route('/getRestaurant', methods=['GET'])
-def getRestaurant(area, type):
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        some_json = request.get_json()
+        return jsonify({'you sent': some_json})
+    else:
+        return jsonify({"about": "Hello Word!"})
 
-    API_KEY = 'APIKEYHERE'
+
+@app.route('/getRestaurant', methods=['GET'])
+def getRestaurant():
+    API_KEY = 'dpkOCmxiu5-igpuEQ7MdTJ69CWqcGiZHpy80Slnt2x8ORRSj8mR_lUrTCrPytfIH8Qau55Mb6fmt1p8oWzma7DfxnYu9sXLZ0O1iZGliz4vVraYnbNEfD60Y1dEFXXYx'
     ENDPOINT = 'https://api.yelp.com/v3/businesses/search'
     HEADERS = {'Authorization': 'bearer %s' % API_KEY}
     contenders = []
 
-    PARAMETERS = {'term': type,
+    PARAMETERS = {'term': 'sushi',
                   'limit': 25,
                   'radius': 10000,
-                  'location': area}
+                  'location': 'san diego'}
 
     response = requests.get(url=ENDPOINT, params=PARAMETERS, headers=HEADERS)
 
@@ -25,20 +33,18 @@ def getRestaurant(area, type):
     for bfz in business_data['businesses']:
         if not bfz['is_closed']:
             contenders.append(bfz)
-            print(bfz)
 
     choice = random.choice(contenders)
     address = ''
     for add in choice['location']['display_address']:
         address += add + ' '
-    print('You should try %s! This has a rating of %d!. The phone number of this restaurant is %s and is located on %s'
-          % (choice['name'], choice['rating'], choice['display_phone'], address))
+    #print('You should try %s! This has a rating of %d!. The phone number of this restaurant is %s and is located on %s'
+          #% (choice['name'], choice['rating'], choice['display_phone'], address))
 
     return jsonify({'choice': choice})
 
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
 
 
 
